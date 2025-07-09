f8488e6bea6c1ade8acb36f2e97a8a5abab04184
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-07-10 00:24:41 +0500

    fix: update favicon link in index.html and add favicon.svg file

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -45,13 +45,16 @@ const Nav = () => {
             <section className="w-full py-5 pr-7   flex justify-between  items-center  sm:px-20 ">
                   <CgMenuRightAlt className="rotate-180 scale-50 sm:scale-100 hidden cursor-pointer" size="2rem" color="#3D5557" />
                   <div className="overflow-hidden flex px-5  sm:px-0 items-center text-center w-[90%]  sm:w-full  pb-1">
-                        <svg className="scale-50 min-[500px]:scale-75 sm:scale-100" width="68" height="56" viewBox="0 0 68 56" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M39.3823 6L56.2698 35.25H22.4948L39.3823 6Z" fill="#4A4646" />
-                              <path d="M32.5 6L49.3875 35.25H15.6125L32.5 6Z" fill="#D9D9D9" />
-                              <circle cx="33.6471" cy="23.2059" r="5.73529" fill="#4A4646" />
-                              <path d="M19 38C19 40.7614 21.2386 43 24 43H44C46.7614 43 49 40.7614 49 38V5C49 2.23858 51.2386 0 54 0H63C65.7614 0 68 2.23858 68 5V51C68 53.7614 65.7614 56 63 56H5C2.23858 56 0 53.7614 0 51V5C0 2.23858 2.23858 0 5 0H14C16.7614 0 19 2.23858 19 5V38Z" fill="#4A4646" />
-                              <rect x="19" y="35" width="30" height="21" fill="#D9D9D9" />
+                        <svg width="70" height="58" viewBox="0 0 70 58" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M40.2121 6.12643L57.4555 35.9927H22.9688L40.2121 6.12643Z" fill="#4A4646" />
+                              <path d="M33.1848 6.12643L50.4281 35.9927H15.9415L33.1848 6.12643Z" fill="#D9D9D9" />
+                              <circle cx="34.356" cy="23.6948" r="5.85614" fill="#4A4646" />
+                              <path d="M0 5C0 2.23858 2.23858 0 5 0H14.4003C17.1618 0 19.4003 2.23858 19.4003 5V57.1799H0V5Z" fill="#4A4646" />
+                              <path d="M50.0324 5C50.0324 2.23858 52.271 0 55.0324 0H64.4327C67.1942 0 69.4327 2.23858 69.4327 5V57.1799H50.0324V5Z" fill="#4A4646" />
+                              <rect x="1.02106" y="43.906" width="67.3906" height="13.2739" rx="5" fill="#4A4646" />
+                              <rect x="19.4004" y="35.7374" width="30.6321" height="21.4425" fill="#D9D9D9" />
                         </svg>
+
                         <div className="flex-11/12">
                               <h1 ref={title2} className="sm:text-2xl  md:text-3xl lg:text-4xl text-[1.5rem]  hidden min-[500px]:text-[1.3rem]  min-[500px]:block  sm:block  font-Astralaga selection:bg-[#3D5557]/60  text-[#3D5557] sm:leading-none  font-semibold uppercase">
                                     way to explore more !
